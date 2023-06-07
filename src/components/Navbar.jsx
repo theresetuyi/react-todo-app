47ffb272b6039c058a41d6fccb961305f90be1b1
unknown <teddytiyi@gmail.com>
2023-06-07 14:04:16 +0200

    fix eslint

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,33 +1,40 @@
 import { useState, useEffect, useRef } from "react";
+
 const Navbar = () => {
-    const [dropdown, setDropdown] = useState(false);
-    useEffect(() => {
-        const handler = (event) => {
-          if (dropdown && ref.current && !ref.current.contains(event.target)) {
-            setDropdown(false);
-          }
-        };
-        document.addEventListener("mousedown", handler);
-      }, [dropdown]);
-    return (
-      <nav>
-        <ul>
-          <li>Home</li>
-          <li>About</li>
-          <li>
+  const [dropdown, setDropdown] = useState(false);
+  const ref = useRef(null); // Create a ref for the dropdown element
+
+  useEffect(() => {
+    const handler = (event) => {
+      if (dropdown && ref.current && !ref.current.contains(event.target)) {
+        setDropdown(false);
+      }
+    };
+    document.addEventListener("mousedown", handler);
+    return () => {
+      document.removeEventListener("mousedown", handler);
+    };
+  }, [dropdown, ref]);
+
+  return (
+    <nav>
+      <ul>
+        <li>Home</li>
+        <li>About</li>
+        <li>
           <button onClick={() => setDropdown(!dropdown)}>
             Services <span>&#8595;</span>
           </button>
           {dropdown && (
-            <ul>
+            <ul ref={ref}>
               <li>Design</li>
               <li>Development</li>
             </ul>
           )}
-          </li>
-        </ul>
-      </nav>
-    );
-  };
-  export default Navbar;
-  
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,10 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { fileURLToPath } from 'url';
 import path from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 export default defineConfig({
   resolve: {
